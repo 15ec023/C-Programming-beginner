85e767a517d4dc7495f9839fb2b3a4a7f7241ba2
15ec023 <priya98.pb@gmail.com>
2017-08-31 14:45:53 +0530

    Create aaa.c

---
+++ aaa.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int i;
+ scanf("%d",i);
+ if(i>0)
+ printf("the given number is positive");
+ else if(i<0)
+ printf("the ginev number is negative");
+ else
+ printf("the given no.is zero");
+ return 0;
+ }
+ 
