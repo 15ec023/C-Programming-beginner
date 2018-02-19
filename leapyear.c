763c1700f610b777202a346170d911fbf3823c83
15ec023 <priya98.pb@gmail.com>
2018-02-19 11:11:08 +0530

    Create leapyear.c

---
+++ leapyear.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+scanf("%d",&a);
+if(a%4==0)
+{
+ if(a%100==0)
+  {
+    if(a%400==0)
+     {
+      printf("Leap year");
+      }}}
+else
+printf("not a Leap year");
+}
