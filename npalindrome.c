469eb84f926c176a210eac23fd6aaf7e5c8f0a12
15ec023 <priya98.pb@gmail.com>
2018-02-20 11:13:51 +0530

    Create npalindrome.c

---
+++ npalindrome.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int n,sum=0,k=n
+ 
+ 
+ 
+ 
+ n=int(raw_input())
+sum=0
+k=n
+while(n!=0):
+
+	a=n%10
+	sum=sum*10+a
+	n=n/10
+if(sum==k):
+	print("palindrome")
+else:
+	print("not palindrome")
